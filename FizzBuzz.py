d754e4ab41536429f5359b7070c9827436195f9c
Aarohi Mangal <aarohishrutimangal@gmail.com>
2020-08-07 23:20:02 +0530

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+numbers=list(range(1,101))
+
+for number in numbers:
+    if(number%3==0 and number%5!=0):
+        print("Fizz")
+    elif(number%5==0 and number%3!=0):
+        print("Buzz")
+    elif(number%5==0 and number%3==0):
+        print("FizzBuzz")
+    else: print(number)
+
+#print (numbers)
