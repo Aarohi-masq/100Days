2a8b052938b86a2920cea2ec1b586cc6cbb15a37
Aarohi Mangal <aarohishrutimangal@gmail.com>
2020-08-09 00:38:43 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,18 @@
+def greet(name):
+    return f"Hey {name}"
+
+print(greet("Mattan"))
+print(greet("Aarohi"))
+
+def concatenate(word_one, word_two):
+    return word_one+word_two
+
+print(concatenate('Me', '+You'))
+
+def age_in_dog_years(age):
+    return age*7
+
+print(age_in_dog_years(19))
+
+def __init__(self):            #constructor
+    #body
