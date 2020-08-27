9ad03de0d0ce5b5f6debd1c85559c1f901139cbd
Aarohi Mangal <aarohishrutimangal@gmail.com>
2020-08-27 22:29:38 +0530

    Update split_and_join.py

--- split_and_join.py
+++ split_and_join.py
@@ -2,3 +2,7 @@ def split_and_join(line):
     split=line.split()
     join = "-".join(split)
     return join
+if __name__ == '__main__':
+    line = raw_input()
+    result = split_and_join(line)
+    print result
